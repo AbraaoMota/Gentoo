d3d155bfbe246640d7f49f22715dc372d90597a6
AbraaoMota <abraaomot@outlook.com>
2018-01-10 12:01:08 +0000

    Update meeting notes, further report progress

--- penguinator/bloop.js
+++ penguinator/bloop.js
@@ -1,8 +1,8 @@
 // <script type="text/javascript">
   // alert("HELLO");
-  var anchors = document.getElementsByTagName("a");
+  // var anchors = document.getElementsByTagName("a");
 
-for (var i = 0; i < anchors.length; i++) {
-  anchors[i].href = "https://www.clubpenguinisland.com/";
-}
+// for (var i = 0; i < anchors.length; i++) {
+//   anchors[i].href = "https://www.clubpenguinisland.com/";
+// }
 // </script>

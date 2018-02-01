1e99a65234a85139107f79c68dbd5f3f07f42979
AbraaoMota <abraaomot@outlook.com>
2018-02-01 17:33:57 +0000

    add popup only when there are inputs in a form

--- extension/src/bloop.js
+++ extension/src/bloop.js
@@ -1,23 +1,15 @@
 window.onload = function() {
   var forms = document.getElementsByTagName("form");
-
-  // console.log(forms);
-  // var inputs = document.getElementsByTagName("input");
-
-  // for (var i = 0; i < inputs.length; i++) {
+  console.log(forms);
   for (var i = 0; i < forms.length; i++) {
     var currForm = forms[i];
     var inputs = currForm.getElementsByTagName("input");
-    // console.log(inputs);
-    // var firstInput = inputs[0];
+    if (!inputs.length)
+      continue;
     var firstInputChild = inputs[0];
-    // console.log(firstChild);
-    // console.log(firstChild.type);
-    // var firstInput = firstChild == null ? document.createTextNode("bloop") : firstChild.innerHTML;
 
     var recommendation = document.createElement('a');
     recommendation.classList.add("recommendation");
-    // recommendation.classList.add("style='display:inline'");
     var text = document.createTextNode("Investigate form");
     recommendation.appendChild(text);
     recommendation.setAttribute("href", "#");
@@ -26,11 +18,6 @@ window.onload = function() {
     });
 
     var newParent = document.createElement("div");
-
-    // currForm.appendChild(newParent);
-    // inputs[i].parentNode.insertBefore(newParent, inputs[i]);
-    // newParent.appendChild(inputs[i]);
-    // firstInput.parentNode.insertBefore(newParent, firstInput);
 
     newParent.appendChild(firstInputChild);
     newParent.appendChild(recommendation);

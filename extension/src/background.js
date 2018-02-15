5d9a755bc6abb2b34c989740c0af7be9c6fad73a
AbraaoMota <abraaomot@outlook.com>
2018-02-15 14:51:07 +0000

    Inject X-XSS-Protection off header

---
+++ extension/src/background.js
@@ -0,0 +1,17 @@
+
+alert("BACKGROUND");
+
+chrome.webRequest.onBeforeSendHeaders.addListener(
+  function(details) {
+    alert("Logging");
+    var antiXSSAuditor = {
+      name: "X-XSS-Protection",
+      value: 0
+    };
+    details.requestHeaders.push(antiXSSAuditor);
+    console.log(details.requestHeaders);
+    // return { details.requestHeaders };
+  },
+  {urls: ["<all_urls>"]},
+  ["blocking", "requestHeaders"]
+);
--- extension/src/bloop.js
+++ extension/src/bloop.js
@@ -1,6 +1,15 @@
+// chrome.webRequest.onBeforeSendHeaders.addListener(
+//   function(details) {
+//     alert("Logging");
+//     console.log(details);
+//   },
+//   {urls: ["<all_urls>"]},
+//   ["blocking"]
+// );
+
 window.onload = function() {
   var forms = document.getElementsByTagName("form");
-  console.log(forms);
+  // console.log(forms);
   for (var i = 0; i < forms.length; i++) {
     var currForm = forms[i];
     var inputs = currForm.getElementsByTagName("input");

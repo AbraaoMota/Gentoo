4ae374b08efa47040480e8304208d80ed9619bf0
AbraaoMota <abraaomot@outlook.com>
2018-02-15 15:13:03 +0000

    add overrides list

--- extension/src/background.js
+++ extension/src/background.js
@@ -3,18 +3,28 @@ alert("BACKGROUND");
 
 chrome.webRequest.onBeforeSendHeaders.addListener(
   function(details) {
-    alert("Logging");
-    var antiXSSAuditor = {
-      name: "X-XSS-Protection",
-      value: "0"
-    };
-    var downGradeInsecureRequests = {
-      name: "Upgrade-Insecure-Requests",
-      value: "0"
-    };
+    var overridesList = [
+      { name: "X-XSS-Protection",          value: "0"},
+      { name: "Upgrade-Insecure-Requests", value: "0"}
+    ];
 
-    details.requestHeaders.push(downGradeInsecureRequests);
-    details.requestHeaders.push(antiXSSAuditor);
+
+    // Find any conflicting headers and remove them
+    for (int i = 0; i < details.requestHeaders.length; i++) {
+      for (int j = 0; j < overridesList.length; j++) {
+        if (details.requestHeaders[i].name == overridesList[j].name) {
+          details.requestHeaders.splice(i, 1);
+        }
+      }
+    }
+
+    // Add in all new headers
+    for (int i = 0; i < overridesList.length; i++) {
+      details.requestHeaders.push(overridesList[i]);
+    }
+
+//     details.requestHeaders.push(downGradeInsecureRequests);
+//     details.requestHeaders.push(antiXSSAuditor);
     console.log(details.requestHeaders);
     // return { details.requestHeaders };
   },
